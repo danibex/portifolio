64d97b7e5bd154e0748b410479cd37c041021481
danibex <danielivam96@gmail.com>
2023-03-06 17:25:49 -0300

    .

---
+++ script.js
@@ -0,0 +1,9 @@
+let img = document.getElementById("portifolio")
+
+    function preVisualizacao() {
+        img.src = "./gifs/googleGlass.gif"
+        img.style.width = "250px"
+    }
+    function voltar(caminhoOld) {
+        img.src=`./img/projetos/protifolio.png`
+    }
