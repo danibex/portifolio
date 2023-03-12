57d7b8ac08cca40ce7151fb41eb55c15d602e9a0
danibex <danielivam96@gmail.com>
2023-03-12 19:20:57 -0300

    .

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
+let body = document.querySelector('body')
 let img = document.getElementById("portifolio")
 let fotoGif = document.querySelectorAll('.fotoGif')
-
+let contateMe = document.querySelector('.contateMe') 
+const url = './form/faleComigo.html' //caminho
 
 fotoGif.forEach((e, index) => {
     let imagem = document.createElement('img') 
@@ -9,3 +11,10 @@ fotoGif.forEach((e, index) => {
     e.appendChild(imagem)
     imagem.src = `./img/projetos/projeto${index + 1}.png`
 })
+
+contateMe.onclick = e => {
+    e.preventDefault()
+    fetch(url)
+        .then(resp => resp.text()) // converte json em objeto
+        .then(html => body.innerHTML = html) 
+}
